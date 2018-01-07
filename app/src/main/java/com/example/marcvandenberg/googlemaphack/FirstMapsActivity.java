74a0377ae1efa00774d9cd1257207a1d23b9a6dd
TJ Kelly <tj.kelly0795@gmail.com>
2018-01-07 15:15:44 -0500

    Added get location funcationality

--- app/src/main/java/com/example/marcvandenberg/googlemaphack/FirstMapsActivity.java
+++ app/src/main/java/com/example/marcvandenberg/googlemaphack/FirstMapsActivity.java
@@ -1,18 +1,34 @@
 package com.example.marcvandenberg.googlemaphack;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class FirstMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    //My vars
+    private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 100;
+    private boolean mLocationPermissionGranted = false;
+    private static final float DEFAULT_ZOOM = 14;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +38,8 @@ public class FirstMapsActivity extends FragmentActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        getLocationPermission();
     }
 
 
@@ -39,8 +57,63 @@ public class FirstMapsActivity extends FragmentActivity implements OnMapReadyCal
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+/*        LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        getDeviceLocationAndSetOnMap();
+
+    }
+
+
+    private void getLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSION_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    private void getDeviceLocationAndSetOnMap() {
+        try {
+            if (mLocationPermissionGranted) {
+                FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                Task locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location result = (Location) task.getResult();
+                            setMapLocation(result.getLatitude(), result.getLongitude());
+                        } else {
+                            Log.e( "empty string","Location Not Available at this time" );
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)
+        {
+            Log.e("PermissionsException: ", e.getMessage());
+        }
+    }
+    private void setMapLocation(double lat, double lon){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( new LatLng(lat, lon), DEFAULT_ZOOM));
+
+        // Add a marker in Sydney and move the camera
+        LatLng newLoc = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(newLoc).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(newLoc));
     }
 }
--- app/src/main/java/com/example/marcvandenberg/googlemaphack/LandingActivity.java
+++ app/src/main/java/com/example/marcvandenberg/googlemaphack/LandingActivity.java
@@ -1,7 +1,13 @@
 package com.example.marcvandenberg.googlemaphack;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +25,7 @@ public class LandingActivity extends AppCompatActivity {
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
     private static final boolean AUTO_HIDE = true;
+
 
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
