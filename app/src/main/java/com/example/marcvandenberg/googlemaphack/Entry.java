45423108ce4827a115cb6e40ab6093306634cf3a
marcvberg <marc.vandenberg1@gmail.com>
2018-03-13 15:02:44 -0400

    added a test xml file

---
+++ app/src/main/java/com/example/marcvandenberg/googlemaphack/Entry.java
@@ -0,0 +1,8 @@
+package com.example.marcvandenberg.googlemaphack;
+
+/**
+ * Created by marcvandenberg on 1/23/18.
+ */
+
+public class Entry {
+}
---
+++ app/src/test/java/com/example/marcvandenberg/googlemaphack/XmlUnitTest.java
@@ -0,0 +1,8 @@
+package com.example.marcvandenberg.googlemaphack;
+
+/**
+ * Created by marcvandenberg on 1/22/18.
+ */
+
+public class XmlUnitTest {
+}
